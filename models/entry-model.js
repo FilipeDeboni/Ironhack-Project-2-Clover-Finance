3f61befa5afd9064789608b7acfb280e5e008b08
D-Kunrath <dkunrath@gmail.com>
2020-09-22 23:45:58 -0300

    set the models and started crud

--- models/entry-model.js
+++ models/entry-model.js
@@ -3,11 +3,10 @@ const { Schema, model } = require("mongoose");
 const entrySchema = new Schema(
   {
     userId: { type: Schema.Types.ObjectId, ref: "Users" },
-    tag: String,
-    amount: Number,
-    category: String,
+    amount: { type: Number, default: 0 },
+    categoryId: { type: Schema.Types.ObjectId, ref: "Categories" },
     desciption: String,
-    date: { type: Date, default: Date.now() }
+    date: { type: Date, required: true, default: Date.now }
   },
   {
     timestamp: true,
